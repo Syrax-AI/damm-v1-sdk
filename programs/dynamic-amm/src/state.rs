72fe3f30d1001ac5730371a2c03ea36b908c29d6
shrimp <55786013+shrimpwtf@users.noreply.github.com>
2025-09-25 21:29:35 +0400

    Update state.rs

--- programs/dynamic-amm/src/state.rs
+++ programs/dynamic-amm/src/state.rs
@@ -56,7 +56,7 @@ impl Default for PoolType {
 }
 
 #[account]
-#[derive(Debug, Copy)]
+#[derive(AnchorSerialize, AnchorDeserialize, Default, Debug, Clone, Copy)]
 /// State of pool account
 pub struct Pool {
     /// LP token mint of the pool
